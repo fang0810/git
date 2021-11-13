c548f5583aa845843bd04c3151746946015c3dd0
fdy <fdy@qq.com>
2021-11-13 20:53:38 +0800

    third git

--- src/main/java/com/fdy/Git.java
+++ src/main/java/com/fdy/Git.java
@@ -10,5 +10,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1111");
+        System.out.println("hello git2222");
     }
 }
